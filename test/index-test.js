dfa7dd61f6cb7c389d70c9f26e0d3bf582710728
mdathrika <mdathrika@ebay.com>
2018-04-02 17:22:02 -0700

    Removed ebay related items

--- test/index-test.js
+++ test/index-test.js
@@ -23,7 +23,7 @@ describe(__filename, function() {
     it('should post metrics to server hook and invoke callback hook', function(done) {
         let payload = {"metrics":{"navType":"1","rc":"0","lt":"1115","ct":"370","rt":"1042"},
         "logs":[{"level":"LOG","msg":"Hello, $logger.log"},{"msg":"Hello,console.log"},{"level":"INFO","msg":"Hello,console.info"},{"level":"WARN","msg":"Hello,console.warn"},{"level":"DEBUG","msg":"Hello,console.debug"},{"level":"ERROR","msg":"Some Error Error: Error Object\n    at logData (http://dathrika.qa.ebay.com:8080/:17:45)\n    at onload (http://dathrika.qa.ebay.com:8080/:19:597)"},{"level":"ERROR","msg":"Uncaught SyntaxError: Unexpected token < in JSON at position 0 http://dathrika.qa.ebay.com:8080/ 1 1 SyntaxError: Unexpected token < in JSON at position 0\n    at JSON.parse (<anonymous>)\n    at XMLHttpRequest.t.onload (https://secureir.ebaystatic.com/cr/v/c1/ghmin.js:1:1864)"}],
-        "ebay":{"rlogid":"t6fgg~hwglm%3F%3CKN(UM%40(7635%3E%3E%3A6-162220b1bf6-0x602","pageName":"DefaultPage"}};
+        "custom":{"pageName":"HomePage"}};
                        
         request.post({url: 'http://localhost:9099/api/log', headers: {'user-agent': 'Chrome', 'referer': 'http://localhost:9099/'},form: payload}, function(err, res, body) {
             payload.url = 'http://localhost:9099/';
@@ -36,7 +36,7 @@ describe(__filename, function() {
     it('should load JS on Browser and fire beacon', function(done) {
                 
         app.on('bowserPayload', (bowserPayload)=> {
-            var pload = {"logs":[{"level":"LOG","msg":"Hello, Logging Data!!"},{"level":"LOG","msg":"Hello from console.log"},{"level":"ERROR","msg":"Hello from console.error"},{"level":"INFO","msg":"Hello from console.info"}],"ebay":{"rlogid":"t6klaook%60b0%3D%3C%3Dosuojbnkmcc4%3B(73766%3F7-161da07d39a-0x602","pageName":"DefaultPage"},"userAgent":"MSIE 9"};
+            var pload = {"logs":[{"level":"LOG","msg":"Hello, Logging Data!!"},{"level":"LOG","msg":"Hello from console.log"},{"level":"ERROR","msg":"Hello from console.error"},{"level":"INFO","msg":"Hello from console.info"}],"custom":{"pageName":"HomePage"},"userAgent":"MSIE 9"};
             assert.equal(bowserPayload, JSON.stringify(pload));
             done();
         });
